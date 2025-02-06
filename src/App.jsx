8ed336a3cc1c3d8f619fae3d18bafcf59e252393
Mohit Kumar Choubey <122045786+MohitCh1@users.noreply.github.com>
2025-02-06 17:09:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Counter from "./components/Counter";
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div id="container">
+         <Counter/>
+      </div>
+      
+  )
+}
+
+export default App
